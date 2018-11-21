d6d40b27d224629d1ff9493130faa4ec86c444a4
Kaitoukiddo <sherrylavender0314@gmail.com>
2018-11-21 08:59:44 -0500

    valid parentheses

--- Solutions.java
+++ Solutions.java
@@ -1,3 +1,4 @@
+//#1 Two Sum
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap <Integer, Integer> map= new HashMap<>();
@@ -8,5 +9,31 @@ class Solution {
             map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No Solution");
+    }
+}
+
+//#3 Valid Parentheses
+class Solution {
+    private HashMap<Character, Character> mappings;
+    
+    public Solution() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put(']', '[');
+        this.mappings.put('}', '{');
+    }
+    
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(this.mappings.containsKey(c)) {
+                char top = stack.empty() ? '#':stack.pop();
+                if(top != this.mappings.get(c)) return false;
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
     }
 }
