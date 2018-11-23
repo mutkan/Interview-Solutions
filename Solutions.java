47b39c98226a1ec514616b7b57d612685bf4de41
Kaitoukiddo <sherrylavender0314@gmail.com>
2018-11-23 10:55:42 -0500

    unique email addresses

--- Solutions.java
+++ Solutions.java
@@ -329,3 +329,22 @@ class Solution2 {
         return dummy.next;
     }
 }
+
+//#929 Unique email addresses
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> str = new HashSet();
+        for(String email: emails) {
+            int i = email.indexOf('@');
+            String local = email.substring(0, i);
+            String rest = email.substring(i);
+            if(local.contains("+")) {
+                local = local.substring(0, local.indexOf('+'));
+            }
+            local = local.replaceAll(".", "");
+            str.add(local+rest);
+        }
+        return str.size();
+    }
+}
+
