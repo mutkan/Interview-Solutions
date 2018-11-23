0a4fb39b5c2ddcd7d17470fc21e042f645c1015f
Kaitoukiddo <sherrylavender0314@gmail.com>
2018-11-23 11:15:33 -0500

    Palindrome number

--- Solutions.java
+++ Solutions.java
@@ -348,3 +348,16 @@ class Solution {
     }
 }
 
+//#9 Palindrome Number
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0)) return false;
+        int revert = 0;
+        while(x>revert) {
+            revert = revert*10 + x%10;
+            x /= 10;
+        }
+        return x==revert || x==revert/10;
+    }
+}
+
