c0c9434ad6ad4913b8d93603b345a2050cc0664a
Kaitoukiddo <sherrylavender0314@gmail.com>
2018-11-24 11:59:10 -0500

    3 sum

--- Solutions.java
+++ Solutions.java
@@ -406,3 +406,32 @@ class Solution2 {
 }
 
 //#15 3 Sum
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-2; i++) {
+            if(i>0 && nums[i]==nums[i-1]) {
+                continue;
+            }
+            int j = i + 1; 
+            int k = nums.length - 1;
+            int target = -nums[i]; //sum 0 
+            while(j<k) {
+                if(nums[j]+nums[k] == target) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                } else if(nums[j]+nums[k] > target) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+}
+
