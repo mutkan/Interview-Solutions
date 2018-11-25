c2650c121798dd76a42a11b27160e8e63512e05b
Kaitoukiddo <sherrylavender0314@gmail.com>
2018-11-24 20:51:16 -0500

    Remove Elements

--- Solutions.java
+++ Solutions.java
@@ -435,3 +435,16 @@ class Solution {
     }
 }
 
+//#27 Remove Elements
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]!=val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
