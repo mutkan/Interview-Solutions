9a2c5fd4fd5f8bb67768e2e6f4c733eedd37f4ad
Kaitoukiddo <sherrylavender0314@gmail.com>
2018-11-29 23:09:16 -0500

    Decode Ways

--- Solutions.java
+++ Solutions.java
@@ -464,3 +464,18 @@ class Solution {
     }
 }
 
+//#91 Decode Ways
+class Solution {
+    public int numDecodings(String s) {
+        if(s.length() == 0) return 0;
+        int[] memo = new int[s.length()+1];
+        memo[s.length()]  = 1;
+        memo[s.length()-1] = s.charAt(s.length()-1) != '0' ? 1 : 0;
+        
+        for (int i = s.length() - 2; i >= 0; i--)
+            if (s.charAt(i) == '0') continue;
+            else memo[i] = (Integer.parseInt(s.substring(i,i+2))<=26) ? memo[i+1]+memo[i+2] : memo[i+1];
+        
+        return memo[0];
+    }
+}
