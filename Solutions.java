81c5f15d3a6c77e96ee32072bc48c2ec6dff84c5
Kaitoukiddo <sherrylavender0314@gmail.com>
2018-12-28 08:22:23 -0500

    valid palindrome

--- Solutions.java
+++ Solutions.java
@@ -493,3 +493,28 @@ class Solution {
     }
 }
 
+
+//#125 Valid Palindrome
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start <= end) {
+            while (start <= end && !Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            }   
+            while (start <= end && !Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            }
+            
+            if (start <= end && Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
+
+
