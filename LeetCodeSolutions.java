e3be1fc1d48172d577e661b9c1dfefd5fb5b584e
Kaitoukiddo <sherrylavender0314@gmail.com>
2019-01-17 06:30:07 -0500

    Kth Largest Element in the Array

--- LeetCodeSolutions.java
+++ LeetCodeSolutions.java
@@ -518,3 +518,66 @@ class Solution {
 }
 
 
+// #215 Kth Largest Element in the Array
+// A smiliar problem starting with order statistics
+// I just watch an hour of two people implementing a sorting algorithm at 3:30 am? I have no life.
+// https://www.youtube.com/watch?v=D35llNtkCps
+
+// QuickSelect has the average runtime O(N)
+class Solution {
+    int[] nums;
+    
+    public int findKthLargest(int[] nums, int k) {
+        this.nums = nums;
+        int size = nums.length;
+        return quickselect(0, size-1, size-k);
+    }
+    
+    public void swap(int a, int b) {
+        int temp = this.nums[a];
+        this.nums[a] = this.nums[b];
+        this.nums[b] = temp;
+    }
+    
+    public int partition(int left, int right, int pivot_index) {
+        int pivot = this.nums[pivot_index];
+        // 1. move pivot to end
+        swap(pivot_index, right);
+        int store_index = left;
+        
+        // 2. move all smaller elements to the left
+        for (int i=left; i<=right; i++) {
+            if (this.nums[i] < pivot) {
+                swap(store_index, i);
+                store_index++;
+            }
+        }
+        
+        // 3. move pivot to its final place
+        swap(store_index, right);
+        
+        return store_index;
+    }
+    
+    public int quickselect(int left, int right, int k_smallest) {
+        // return k-th smallest element of list
+        if (left == right)  return this.nums[left];
+        
+        Random random_num = new Random();
+        int pivot_index = left + random_num.nextInt(right - left);
+        
+        pivot_index = partition(left, right, pivot_index);
+        
+        //pivot is on (N-k)th smallest place
+        if (k_smallest == pivot_index) {
+            return this.nums[k_smallest];
+        } else if (k_smallest < pivot_index) {
+            //left side
+            return quickselect(left, pivot_index-1, k_smallest);
+        } else {
+            return quickselect(pivot_index+1, right, k_smallest);
+        }     
+        
+    }
+}
+
