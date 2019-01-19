13dcf25e62c0ac004220809f28ea96eaaadbd1b1
Kaitoukiddo <sherrylavender0314@gmail.com>
2019-01-19 00:00:05 -0500

    valid sudoku

--- LeetCodeSolutions.java
+++ LeetCodeSolutions.java
@@ -601,3 +601,50 @@ class Solution2 {
     }
 }
 
+
+// #36 Valid Sudoku
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[] visited = new boolean[9];
+        
+        for (int i=0; i<9; i++) {
+            Arrays.fill(visited, false);
+            for (int j=0; j<9; j++) {
+                if (!process(visited, board[i][j]))  
+                    return false;
+            }
+        }
+        
+        for (int i=0; i<9; i++) {
+            Arrays.fill(visited, false);
+            for (int j=0; j<9; j++) {
+                if (!process(visited, board[j][i]))     
+                    return false;
+            }
+        }
+        
+        for (int i=0; i<9; i++) {
+            for (int j=0; i<9; j++) {
+                Arrays.fill(visited, false);
+                for (int k=0; k<9; k++) {
+                    if (!process(visited, board[i+k/3][j+k/3])) 
+                        return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    public boolean process(boolean[] visited, char digit) {
+        if (digit == '.')
+            return true;
+        
+        int num = digit - '0';    
+        if (num < 1 || num > 9 || visited[num-1]) 
+            return false;
+        
+        visited[num-1] = true;
+        return true;
+    }
+}
