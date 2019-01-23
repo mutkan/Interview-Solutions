bb28082fd2ac4519df876f4f52dea70ec38e9811
Kaitoukiddo <sherrylavender0314@gmail.com>
2019-01-22 20:37:12 -0500

    Linked List Cycle II

--- LeetCodeSolutions.java
+++ LeetCodeSolutions.java
@@ -879,3 +879,60 @@ public class Solution {
     }
 }
 
+
+// #142 Linked List Cycle II
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution1 {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> visited = new HashSet<>();
+        ListNode current = head;
+        while (current != null) {
+            if (visited.contains(current)) {
+                return current;
+            }
+            visited.add(current);
+            current = current.next;
+        }
+        return null;
+    }
+}
+
+
+public class Solution2 {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null)  return null;
+        ListNode intersect = findCycle(head);
+        if (intersect == null) return null;
+        
+        ListNode first = head;
+        ListNode second = intersect;
+        while (first != second) {
+            first = first.next;
+            second = second.next;
+        }
+        return first;
+    }
+    
+    public ListNode findCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast)   return slow;
+        }
+        return null;
+    }
+}
+
